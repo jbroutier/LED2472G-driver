5a265057673d3044ba9baf1271b9c725fba2f26d
Jérémie Broutier <jeremie.broutier@gmail.com>
2018-10-15 22:28:44 +0200

    Convert tabs to spaces.

--- LED2472G.h
+++ LED2472G.h
@@ -15,18 +15,18 @@ static constexpr auto LED2472G_DEVICE_ADDRESS = 0X46;
 class LED2472G
 {
 public:
-	explicit LED2472G(const char* devicePath, uint8_t deviceAddress = LED2472G_DEVICE_ADDRESS);
-	virtual ~LED2472G();
+    explicit LED2472G(const char* devicePath, uint8_t deviceAddress = LED2472G_DEVICE_ADDRESS);
+    virtual ~LED2472G();
 
-	void clear();
-	void update();
+    void clear();
+    void update();
 
-	void setPixel(uint8_t x, uint8_t y, uint16_t color);
-	void setPixelRGB(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b);
+    void setPixel(uint8_t x, uint8_t y, uint16_t color);
+    void setPixelRGB(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b);
 
 private:
-	int device;
-	uint8_t pixels[192] = { 0 };
+    int device;
+    uint8_t pixels[192] = { 0 };
 };
 
 #endif // #ifndef LED2472G_H
