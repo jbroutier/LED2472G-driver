474684f3d9b3b27bc8ca278e0d0341776b999f03
Jérémie Broutier <jeremie.broutier@gmail.com>
2018-10-15 14:49:20 +0200

    Initial commit.

---
+++ LED2472G.cpp
@@ -0,0 +1,53 @@
+#include "LED2472G.h"
+
+LED2472G::LED2472G(const char* devicePath, uint8_t deviceAddress)
+{
+	if ((device = open(devicePath, O_RDWR)) < 0) {
+		throw std::runtime_error("Failed to open the I2C bus.");
+	}
+
+	if (ioctl(device, I2C_SLAVE_FORCE, deviceAddress) < 0) {
+		close(device);
+		throw std::runtime_error("Failed to configure the I2C device.");
+	}
+}
+
+void LED2472G::clear()
+{
+	for (uint8_t pixel = 0; pixel < 192; pixel++) {
+		i2c_smbus_write_byte_data(device, pixel, 0);
+	}
+}
+
+void LED2472G::setPixel(uint8_t x, uint8_t y, uint16_t color)
+{
+	uint8_t pixel = (uint8_t)(x * 24 + y);
+
+	pixels[pixel]	   = (uint8_t)((color >> 10) & 0x3E);
+	pixels[pixel + 8]  = (uint8_t)((color >> 5)  & 0x3F);
+	pixels[pixel + 16] = (uint8_t)((color << 1)  & 0x3E);
+}
+
+void LED2472G::setPixelRGB(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b)
+{
+	uint16_t r16 = (r >> 3) & 0x1F;
+	uint16_t g16 = (g >> 2) & 0x3F;
+	uint16_t b16 = (b >> 3) & 0x1F;
+
+	uint16_t color = static_cast<uint16_t>((r16 << 11) + (g16 << 5) + b16);
+
+	setPixel(x, y, color);
+}
+
+void LED2472G::update()
+{
+	for (uint8_t pixel = 0; pixel < 192; pixel++) {
+		uint8_t color = pixels[pixel];
+		i2c_smbus_write_byte_data(device, pixel, color);
+	}
+}
+
+LED2472G::~LED2472G()
+{
+	close(device);
+}
---
+++ LED2472G.h
@@ -0,0 +1,32 @@
+#ifndef LED2472G_H
+#define LED2472G_H
+
+#include <stdexcept>
+
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+static constexpr auto LED2472G_DEVICE_ADDRESS = 0X46;
+
+class LED2472G
+{
+public:
+	explicit LED2472G(const char* devicePath, uint8_t deviceAddress = LED2472G_DEVICE_ADDRESS);
+	virtual ~LED2472G();
+
+	void clear();
+	void update();
+
+	void setPixel(uint8_t x, uint8_t y, uint16_t color);
+	void setPixelRGB(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b);
+
+private:
+	int device;
+	uint8_t pixels[192] = { 0 };
+};
+
+#endif // #ifndef LED2472G_H
